270cefd71df3e5369b2ff7be37c3db30821ad746
Robert Wilson <robert.wilson1717@gmail.com>
2013-11-02 14:42:07 +0000

    OAuth

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
-var path = require('path');
+var passport = require('passport'),
+util = require('util'),
+GitHubStrategy = require('passport-github').Strategy,
+path = require('path'),
+config = require('./config.js');
 
 var app = express();
 
@@ -19,6 +22,8 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,8 +32,52 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+passport.serializeUser(function(user, done) {
+        done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+        done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: config.githubID,
+    clientSecret: config.githubSecret,
+    callbackURL: config.githuibCallback
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// Auth Routes
+
+app.get('/auth/github',
+passport.authenticate('github'),
+function(req, res){
+});
+
+app.get('/auth/github/callback', 
+passport.authenticate('github', { failureRedirect: '/login' }),
+function(req, res) {
+res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
