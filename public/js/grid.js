90289a227c254f6a135edc9acd61a57db9e87f61
Robert Wilson <robert.wilson1717@gmail.com>
2013-11-02 20:28:07 +0000

    Add Grid :S

---
+++ public/js/grid.js
@@ -0,0 +1,51 @@
+//define screen size
+var screenSize = 120;
+
+//define rect size
+var cubeSize = 20;
+
+var svg = "";
+var grid = grid || {};
+
+var la = "";
+var lo = "";
+
+var dataurl = "http://api.geeksoc-hackathon.tk/data/map.php"
+
+	grid.setup = function (s) {
+	svg = var svg = d3.select("#" + s).append("svg")
+		.attr("width", screenSize)
+		.attr("height", screenSize);
+	)
+};
+
+grid.updateGrid = function (lat, lon) {
+	$.ajax({
+		url : dataurl + "?lat=" + lat + "&lon=" + lon,
+		type : "GET",
+		dataType : "json",
+		success : onDataReceived
+	});
+};
+
+function onDataReceived(data) {
+	console.log(data);
+	for (var i = 0; i < data.length; i++) {
+		svg.selectAll("rect")
+		.data(data[i])
+		.enter()
+		.append("rect")
+		.attr("x", i % 5)
+		.attr("y", (i - (i % 5)) / 5))
+		.on("click", function (d) {
+			d3.select("#value").text(d[1])
+		})
+		.on("mouseover", function () {
+			d3.select(this).style("fill", "aliceblue");
+		})
+		.on("mouseout", function () {
+			d3.select(this).style("fill", "white");
+		});
+	}
+
+};
