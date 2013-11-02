94dc7ebb152ed85145803f430b7cb4be3be7b476
Robert Wilson <robert.wilson1717@gmail.com>
2013-11-02 16:45:13 +0000

    Test of D3 and data

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,35 @@
 /*
  * GET home page.
  */
+ var mysql = require('mysql')
+ config = require('./config.js');
+ 
+ var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : config.mysqlUser,
+        password : config.mysqlPass
+    });
 
 exports.index = function(req, res){
-  res.render('index', { title: 'NetDefense' ,user: req.user});
+connection.query('SELECT `CHANNEL` FROM `aps` ;', req.body, 
+        function (err, results) {
+            if (err) throw err;
+            var chans = new array();
+            for(var i = 1; i < results.length; i++){
+            chans[results[i].channel] = chans[results[i].channel] + 1;
+            
+            }
+            for(var i = 1; i < chans.length; i++)
+                {
+                        data.push({
+                                index: i,
+                                name: 'test',
+                                value: getRandomInt(0,1000)
+                        });
+                }
+            res.render('index', { title: 'NetDefense' ,user: req.user, data:results});
+        }
+    );
+
+  
 };
