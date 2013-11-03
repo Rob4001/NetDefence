862bca9c27ce29af58ad2e42be75c3cbae19367c
Robert Wilson <robert.wilson1717@gmail.com>
2013-11-03 10:31:20 +0000

    Remove derp

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,12 @@
 /*
  * GET home page.
  */
- var mysql = require('mysql')
- config = require('../config.js');
- 
- var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : config.mysqlUser,
-        password : config.mysqlPass
-    });
-connection.query('USE loaded', function (err) {
-        if (err) throw err;
-});
-exports.index = function(req, res){
-connection.query('SELECT `CHANNEL` FROM `aps`',
-        function (err, results) {
-            if (err) throw err;
-            var chans = new Array();
-            for(var i = 1; i < results.length; i++){
-            if(chans[results[i].CHANNEL]){
-            chans[results[i].CHANNEL]++;
-            }else{
-            
-            chans[results[i].CHANNEL] = 1;
-            }
-            }
-            var data = [];
-            for(var index in chans)
-                {
-                
-                        data.push({
-                                label: index,
-                                value: chans[index]
-                        });
-                }
-                console.log(data)
-            res.render('index', { title: 'NetDefense' ,user: req.user, data: JSON.stringify(data)});
-        }
-    );
 
-  
+exports.index = function (req, res) {
+	res.render('index', {
+		title : 'NetDefense',
+		user : req.user,
+		data :
+	});
+
 };
